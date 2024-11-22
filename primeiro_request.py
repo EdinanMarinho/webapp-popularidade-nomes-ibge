d599db3f329c73ee8b972c1c51d718c388e4ce01
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-11-22 17:48:25 -0300

    teste de request com meu site

--- primeiro_request.py
+++ primeiro_request.py
@@ -1,11 +1,11 @@
 import requests
 
-url = "https://www.google.com"
+url = "https://edinanmarinho.com.br"
 
 resposta = requests.get(url)
 
 print(resposta)
 print(resposta.text)
 
-with open('pagina_google.html', 'w') as arquivo:
-    arquivo.write(resposta.text)
+# with open('pagina_google.html', 'w') as arquivo:
+#     arquivo.write(resposta.text)
