3b2102320320d9a5a88d73c464be6764791e555c
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-11-23 17:51:31 -0300

    request test

--- primeiro_request.py
+++ primeiro_request.py
@@ -1,9 +1,9 @@
 import requests
 
-url = "https://google.com?param1=valor1"
+url = "https://google.com"
 
 resposta = requests.get(url)
 
 print(resposta)
-print(resposta.text)
+# print(resposta.text)
 
