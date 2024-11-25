3dd7d752261dd233bcd36b780580762fe5b68abf
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-11-25 09:07:40 -0300

    testando metodo post

--- gerando_requests.py
+++ gerando_requests.py
@@ -1,6 +1,15 @@
 import requests
 
-url = 'https://httpbin.org/get'
-resposta = requests.get(url)
+url = 'https://httpbin.org/post'
+
+data = {
+    "meus_dados": [1, 2, 3],
+    "pessoa": {
+        "nome": "Edinan",
+        "Business Analyst Professional - Data Science": True
+    }
+}
+
+resposta = requests.post(url, json=data)
 
 print( resposta.json() )
