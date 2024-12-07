b2636e6dc6f06e8fa653d3ff74ac940de83c4200
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-12-07 12:13:16 -0300

    finalizado codigo combinado apis exit() exit
    exit
    exit()
    :wq
    
    :wq
    q!+

--- combinando_requests.py
+++ combinando_requests.py
@@ -13,7 +13,20 @@ def pegar_ids_estados():
         id_estado = dados['UF-id'] # extrair ids do estado
         nome_estado = dados['UF-nome'] # extrair nome estado
         dict_estado[id_estado] = nome_estado
-    return dict_estado     
+    return dict_estado   
+
+def pegar_frequencia_nome_por_estado(nome):
+    url = f"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}"
+    params = {
+        'groupBy':'UF',
+    }
+    dados_frequencias = fazer_request(url=url, params=params)
+    dict_frequencias = {}
+    for dados in dados_frequencias:
+        id_estado = int(dados['localidade'])
+        frequencia = dados['res'][0]['proporcao']
+        dict_frequencias[id_estado] =  frequencia
+    return dict_frequencias
 
 
 def fazer_request(url, params=None):
@@ -28,9 +41,13 @@ def fazer_request(url, params=None):
     return resultado
 
 
-def main():
+def main(nome):
     dict_estados = pegar_ids_estados()
-    pprint(dict_estados)
+    dict_frequencia =  pegar_frequencia_nome_por_estado(nome)
+    print(f'Frequencia do nome {nome} nos Estados (por 100.000 habitantes)')
+    for id_estado, nome_estado in dict_estados.items():
+        frequencia_estado = dict_frequencia[id_estado]
+        print( f'--> {nome_estado}: {frequencia_estado}' )
 
 if __name__ == '__main__':
-    main()
+    main('Rafaela')
