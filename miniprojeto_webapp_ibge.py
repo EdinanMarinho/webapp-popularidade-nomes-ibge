9b173718d73663b797664e54a159f49e242849c6
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-12-13 16:00:22 -0300

    atualizado codigo

--- miniprojeto_webapp_ibge.py
+++ miniprojeto_webapp_ibge.py
@@ -1,8 +1,11 @@
 import requests
+import pandas    as pd
+import streamlit as st 
+
 from pprint import pprint
 
 def fazer_request(url, params=None):
-    resposta = requests.get(url, params=params)
+    resposta = requests.get(url=url, params=params)
     try:
         resposta.raise_for_status()
     except requests.HTTPError as e:
@@ -17,9 +20,22 @@ def pegar_nome_por_decada(nome):
     url = f"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}"
     dados_decadas = fazer_request( url=url )
     print(dados_decadas)
+    dict_decadas = {}
+    for dados in dados_decadas[0]['res']:
+        decada = dados['periodo']
+        quantidade = dados['frequencia']
+        dict_decadas[decada] =  quantidade
+    return dict_decadas
 
-def main(nome):
-    pegar_nome_por_decada(nome)
+def main():
+    st.title('Web App Nomes')
+    st.write('Dados do IBGE (fonte: https://servicodados.ibge.gov.br/api/docs/nomes?versao=2)')
+    nome  = st.text_input('Consulte um nome:')
+    if not nome:
+        st.stop()
+    
+    dict_decadas = pegar_nome_por_decada(nome)
+    pprint(dict_decadas)
 
 if __name__ == '__main__':
-    main('Rafaela')
+    main()
