a5ca7f3de1e81617e4440e720179114503efa5ee
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-12-17 15:04:35 -0300

    finalizado web app

--- miniprojeto_webapp_ibge.py
+++ miniprojeto_webapp_ibge.py
@@ -15,10 +15,11 @@ def fazer_request(url, params=None):
         resultado = resposta.json()
     return resultado
 
-
 def pegar_nome_por_decada(nome):
     url = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}'
     dados_decadas = fazer_request( url=url )
+    if not dados_decadas:
+        return {}
     dict_decadas = {}
     for dados in dados_decadas[0]['res']:
         decada = dados['periodo']
@@ -26,17 +27,28 @@ def pegar_nome_por_decada(nome):
         dict_decadas[decada] =  quantidade
     return dict_decadas
 
-
 def main():
     st.title('Web App Nomes')
     st.write('Dados do IBGE (fonte: https://servicodados.ibge.gov.br/api/docs/nomes?versao=2)')
+    
     nome  = st.text_input('Consulte um nome:')
     if not nome:
         st.stop()
+    
     dict_decadas = pegar_nome_por_decada(nome)
+    if not dict_decadas:
+        st.warning(f'Nenhum dado encontrado para o nome {nome}')
+        st.stop()
+
     df = pd.DataFrame.from_dict(dict_decadas, orient='index')
 
-    st.dataframe(df)
+    col1, col2 = st.columns([0.3, 0.7])
+    with col1:
+        st.write('Frequencia por década')
+        st.dataframe(df)
+    with col2:
+        st.write('Evolução no Tempo')
+        st.line_chart(df)
 
 if __name__ == '__main__':
     main()
