f908157e1ed9127575654d6f40873c1b2830f77e
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-12-18 14:51:01 -0300

    testado erro 401 request

--- auth_basica.py
+++ auth_basica.py
@@ -1,10 +1,27 @@
 import base64
+import requests
+from pprint import pprint
 
 usuario = "meu-usuario"
-senha   = "senha-secreta"
+senha   = "senha-errada"
 
 auth_string = f'{usuario}:{senha}'.encode()
 auth_string = base64.b64encode(auth_string)
 auth_string = auth_string.decode()
 
-print(auth_string)
+url = 'https://httpbin.org/basic-auth/meu-usuario/senha-secreta'
+headers = {
+    'Authorization': f'Basic {auth_string}'
+}
+
+resposta = requests.get(url=url, headers=headers)
+
+try:
+    resposta.raise_for_status()
+except requests.HTTPError as e:
+    print(f'Erro no request{e}')
+    resultado = None
+else:
+    resultado = resposta.json()
+
+pprint(resultado)
