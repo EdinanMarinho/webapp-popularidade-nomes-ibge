6b6254820d24d84f90c4f8edcc8dbc946beddee6
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2025-01-20 15:01:15 -0300

    Finalizado miniprojeto web app clima tempo

--- miniprojeto_webapp_openweather.py
+++ miniprojeto_webapp_openweather.py
@@ -47,9 +47,17 @@ def main():
     temperatura = dados_tempo['main']['temp']
     sensacao_termica = dados_tempo['main']['feels_like']
     umidade = dados_tempo['main']['humidity']
-    cobertura_nuvens = dados_tempo['main']['clouds']['all']
-    
-    print(dados_tempo)
+    cobertura_nuvens = dados_tempo['clouds']['all']
+
+    st.metric(label='Tempo atual', value=clima_atual)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric(label='Temperatura', value=f'{temperatura} °C')
+        st.metric(label='Sensação Térmica', value=f'{sensacao_termica} °C')
+    with col2:
+        st.metric(label='Umidade', value=f'{umidade} %')
+        st.metric(label='Cobertura de Nuvens', value=f'{cobertura_nuvens} %')
+
 
 if __name__ == '__main__':
     main()
