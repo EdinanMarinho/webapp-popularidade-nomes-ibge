cd1f5dddd53b53e996002570ae6983b4555cc710
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2025-01-23 14:58:17 -0300

    Iniciado miniprojeto spotify

---
+++ miniprojeto_webapp_spotify.py
@@ -0,0 +1,41 @@
+import os
+
+import requests
+from requests.auth import HTTPBasicAuth
+from pprint import pprint
+import dotenv
+
+dotenv.load_dotenv()
+
+def autenticar():
+    client_id = os.environ['SPOTIFY_CLIENT_ID']
+    client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+
+    auth = HTTPBasicAuth( username=client_id, password=client_secret )
+
+    url = "https://accounts.spotify.com/api/token"
+    body = {
+        "grant_type": "client_credentials"
+    }
+
+    resposta = requests.post( url=url, data=body, auth=auth )
+
+    try:
+        resposta.raise_for_status()
+    except requests.HTTPError as e:
+        print(f'Erro no request{e}')
+        token = None
+    else:
+        token = resposta.json()['access_token']
+        print('Token obtido com sucesso!')
+    return token
+
+id_artista = "1CFCsEqKrCyvAFKOATQHiW"
+
+url = f"https://api.spotify.com/v1/artists/{id_artista}"
+headers = {
+    'Authorization': f'Bearer {token}'
+}
+
+resposta = requests.get( url=url, headers=headers )
+pprint( resposta.json())
